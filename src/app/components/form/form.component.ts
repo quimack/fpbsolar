325ce57c8854b4792972b2ee6ee531e2b012f925
macarena <macarena@blockinar.com>
2023-11-01 20:49:43 -0300

    Comienza formulario

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InstallationType } from '../../models/installationType';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+  
+  step1: boolean = true;
+  step2: boolean = false;
+
+  installationType: InstallationType = InstallationType.HOGAR;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.installationType);
+  }
+
+  setInstallationType(typeId: InstallationType){
+    this.installationType = typeId;
+
+    this.step1 = false;
+    this.step2 = true;
+
   }
 
 }
