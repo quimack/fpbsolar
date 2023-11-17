f6f6c0ecda40ce8ea47e994033d44903a4280108
Diego Rosso <diegorosso1988@gmail.com>
2023-11-17 18:32:39 -0300

    responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,27 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import * as AOS from "aos";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fpbsolar';
     showArrow: boolean = false;
 
     @HostListener('window:scroll', ['$event'])
     onScroll(event: Event): void {
-      console.log("scrooool", window.scrollY)
       if(window.scrollY >= 1100){
         this.showArrow = true;
-        console.log(this.showArrow)
       }else{
         this.showArrow = false;
       }
+    }
+
+    ngOnInit(){
+      AOS.init();
+      window.addEventListener("load", AOS.refresh)
     }
 
 }
